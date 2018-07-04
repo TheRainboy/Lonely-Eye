97d3f8884a4e4017eae32cc65796eafa37e2a21f
TheRainboy <midge433@outlook.com>
2018-07-04 13:25:29 +0100

    Functional birds
    There are now birds with animations, they automatically move and they spawn automatically. They also kill you

---
+++ Lonely eye/Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour {
+
+    public float speed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate (-speed * Time.deltaTime, 0, 0f); //if it is -speed you go left
+	}
+}
