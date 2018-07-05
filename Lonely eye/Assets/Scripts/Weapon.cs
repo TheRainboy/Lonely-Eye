2556ac3cb39d5e6e8d45713377fdc4992256d321
TheRainboy <midge433@outlook.com>
2018-07-05 12:29:39 +0100

    Working shooting mechanic
    You now shoot tears

--- Lonely eye/Assets/Scripts/Weapon.cs
+++ Lonely eye/Assets/Scripts/Weapon.cs
@@ -6,7 +6,8 @@ public class Weapon : MonoBehaviour {
 
     public float fireRate = 0;
     public float Damage = 10;
-    public LayerMask notToHit;
+    public LayerMask whatToHit;
+    public Transform BulletTrailPrefab;
 
     float timeToFire = 0f;
     Transform firePoint;
@@ -24,15 +25,14 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
     void Update(){
-        Shoot();
         if(fireRate == 0){
-            if (Input.GetButtonDown("Fire1")){
+            if (Input.GetKeyDown(KeyCode.Q)){
                 Shoot();
             }
         }
 
         else{
-            if (Input.GetButton("Fire1") && Time.time > timeToFire){
+            if (Input.GetKey(KeyCode.Q) && Time.time > timeToFire){
                 timeToFire = Time.time + 1/fireRate;
                 Shoot();
             }
@@ -42,7 +42,16 @@ public class Weapon : MonoBehaviour {
     void Shoot(){
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, notToHit);
-        Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition) *100);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+        Effect();
+        Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition) *100, Color.cyan);
+        if (hit.collider != null){
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log(" We hit" + hit.collider.name + " and dealt" + Damage + "damage.");
+        }
+    }
+
+    void Effect(){
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
     }
 }
