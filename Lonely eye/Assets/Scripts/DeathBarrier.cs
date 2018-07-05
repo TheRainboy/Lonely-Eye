77b6c3b557c7fd78801a5f82aee3b0038fa730ed
TheRainboy <midge433@outlook.com>
2018-07-05 21:50:08 +0100

    Revert "Revert "more developed level""
    This reverts commit 8fb47e771b4a96cf4d061c820b6d52a9dc5c28dd.

---
+++ Lonely eye/Assets/Scripts/DeathBarrier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour {
+
+    public GameObject player;
+    public Transform spawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        player.transform.position = spawnPoint.transform.position;
+    }
+}
