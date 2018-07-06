dfae3b62f8b7ec920a9b47dca14ebc7c114725e1
TheRainboy <midge433@outlook.com>
2018-07-06 15:21:48 +0100

    more features

---
+++ Lonely eye/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource TearAudio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TearAudio.Play();
+        }
+	}
+}
---
+++ Lonely eye/Assets/Scripts/ReadSign.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadSign : MonoBehaviour
+{
+
+    public GameObject UIobject;
+    public GameObject player;
+
+    private void Start()
+    {
+        UIobject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        StartCoroutine(WaitForSec());
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            UIobject.SetActive(true);
+
+        }
+
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(UIobject);
+        Destroy(gameObject);
+    }
+}
+    
